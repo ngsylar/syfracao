f11c6c02e496efcc12a5a6d57d6e4823330ddca4
ngsylar <ngsylar@gmail.com>
2023-06-29 21:32:18 -0300

    RSA OAEP

--- OAEP.py
+++ OAEP.py
@@ -16,7 +16,7 @@ def MGF1 (seed: bytearray, length: int, hash_func=hashlib.sha3_256) -> bytearray
         counter += 1
     return bytearray(T[:length])
 
-def PadMessage (publicKey: tuple[int, int], messsage: bytearray, label: bytearray=bytearray()) -> bytearray:
+def Pad (publicKey: tuple[int, int], messsage: bytearray, label: bytearray=bytearray()) -> bytearray:
     (modulus, _) = publicKey
     labelHash = bytearray(hashlib.sha3_256(bytes(label)).digest())
 
@@ -37,7 +37,7 @@ def PadMessage (publicKey: tuple[int, int], messsage: bytearray, label: bytearra
     paddedMsg = bytearray(b'\x00') + maskedSeed + maskedDB
     return paddedMsg
 
-def UnpadMessage (paddedMsg: bytearray, label: bytearray=bytearray()) -> bytearray:
+def Unpad (paddedMsg: bytearray, label: bytearray=bytearray()) -> bytearray:
     if (paddedMsg[0] != 0x00):
         print("OAEP Error: first byte is non-zero")
         return bytearray(0)
@@ -73,21 +73,3 @@ def XorBlock (dominantBlock: bytearray, recessiveBlock: bytearray) -> bytearray:
     for i in range(len(xoredBlock)):
         xoredBlock[i] = dominantBlock[i] ^ recessiveBlock[i]
     return xoredBlock
-
-# # teste
-# import millerrabin
-# E_DEF = 65537
-
-# labelT = "Gabriel F., 27at2301"
-# messageT = "A Hello, World! program is generally a computer program that ignores any input, and outputs or displays a message similar to Hello, World!"
-
-# p = millerrabin.GenPrime(1024)
-# q = millerrabin.GenPrime(1024)
-# n = p * q
-
-# senderT = PadMessage((n, E_DEF), convert.str_to_bytearray(messageT), convert.str_to_bytearray(labelT))
-# # senderT[6] ^= 0b00001000
-# receiverT = UnpadMessage(senderT, convert.str_to_bytearray(labelT))
-
-# print(convert.bytearray_to_str(receiverT))
-# print(bytearray())
--- RSA.py
+++ RSA.py
@@ -4,11 +4,12 @@ import OAEP
 from utilities import Qualities as quality, Conversions as convert, PseudoRandom as random
 
 PUBLIC_EXP_DEFAULT = 65537
-__msgByteCountDef = 254 - (2 * OAEP.HASH_BYTE_COUNT)
+PRIME_BIT_COUNT_MIN = 1024
 
-def GenerateKeys (msgByteCount: int=__msgByteCountDef):
-    if (msgByteCount < __msgByteCountDef):
-        msgByteCount = __msgByteCountDef
+def GenerateKeys (msgByteCount: int=0):
+    msgByteCountDef = ((2*PRIME_BIT_COUNT_MIN + 7) // 8) - (2*OAEP.HASH_BYTE_COUNT) - 2
+    if (msgByteCount < msgByteCountDef):
+        msgByteCount = msgByteCountDef
 
     keyBitCountMax = (msgByteCount + 2*OAEP.HASH_BYTE_COUNT + 2) * 8
     primeBitCount = (keyBitCountMax + 1) // 2
@@ -29,21 +30,38 @@ def GenerateKeys (msgByteCount: int=__msgByteCountDef):
         publicExp = random.int_in_range(3, lambdaN)
     privateExp = pow(publicExp, -1, lambdaN)
 
-    return ((modulus, publicExp), privateExp)
+    return (modulus, publicExp), privateExp
+
+def Cipher (publicKey: tuple[int, int], message: str, label: str) -> int:
+    plainText = convert.str_to_bytearray(message)
+    labelText = convert.str_to_bytearray(label)
+    (modulus, publicExp) = publicKey
+
+    paddedMsg = OAEP.Pad(publicKey, plainText, labelText)
+    paddedMsg = convert.bytearray_to_int(paddedMsg)
+    cipher = pow(paddedMsg, publicExp, modulus)
+
+    return cipher
+
+def Decipher (privateKey: int, publicKey: tuple[int, int], cipher: int, label: str) -> str:
+    (modulus, _) = publicKey
+    privateExp = privateKey
+
+    decipher = pow(cipher, privateExp, modulus)
+    paddedDec = bytearray(1) + convert.int_to_bytearray(decipher)
+    labelText = convert.str_to_bytearray(label)
+
+    decipherText = OAEP.Unpad(paddedDec, labelText)
+    message = convert.bytearray_to_str(decipherText)
+
+    return message
 
 # teste
 labelT = "Gabriel F., 27at2301"
 messageT = "A Hello, World! program is generally a computer program that ignores any input, and outputs or displays a message similar to Hello, World!"
 
-((nT, eT), dT) = GenerateKeys(len(messageT))
-msgTInt = convert.str_to_int(messageT)
+publicKey, privateKey = GenerateKeys(len(messageT))
+cipherT = Cipher(publicKey, messageT, labelT)
+decipherT = Decipher(privateKey, publicKey, cipherT, labelT)
 
-print(quality.count_bytes_of_int(nT))
-print(quality.count_bytes_of_int(msgTInt))
-
-paddedMsgT = OAEP.PadMessage((nT, eT), convert.str_to_bytearray(messageT), convert.str_to_bytearray(labelT))
-
-cipherT = pow(convert.bytearray_to_int(paddedMsgT), eT, nT)
-decipherT = convert.int_to_bytearray(pow(cipherT, dT, nT))
-
-print(OAEP.UnpadMessage(bytearray(1) + decipherT, convert.str_to_bytearray(labelT)))
+print(decipherT)
